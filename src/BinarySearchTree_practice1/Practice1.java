3ba55105c0b507d696963912b74c0703273845d7
hojipkim <rlaghwlq12@gmail.com>
2022-10-12 15:33:08 +0900

    Commit(2022.09.27)

---
+++ src/BinarySearchTree_practice1/Practice1.java
@@ -0,0 +1,20 @@
+package BinarySearchTree_practice1;
+
+
+//Practice1
+//주어진 이진 탐색 트리에서 N번째로 작은 수 구하기.
+
+//입력 트리 : 3, 1, 4, null, 2
+//N : 1
+//결과 : 1
+
+//입력 트리 : 5, 3, 6, 2, 4, null, null, 1
+//N : 3
+//결과 : 3
+
+public class Practice1 {
+
+
+
+
+}
